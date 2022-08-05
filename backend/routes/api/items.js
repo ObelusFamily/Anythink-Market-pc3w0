7a364afe84ec300257d5c982f9126ec6623404ea
puru-khedre <101047627+puru-khedre@users.noreply.github.com>
2022-08-05 14:37:57 +0530

    Update items.js
    merge in your branch and then commit

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -14,7 +14,8 @@ router.param("item", function(req, res, next, slug) {
       if (!item) {
         return res.sendStatus(404);
       }
-
+      
+      if(item.image === "") item.image = "http://localhost:3001/placeholder.png";
       req.item = item;
 
       return next();
@@ -147,7 +148,6 @@ router.post("/", auth.required, function(req, res, next) {
       var item = new Item(req.body.item);
 
       item.seller = user;
-      items.image?items.image:"placeholder"
 
       return item.save().then(function() {
         sendEvent('item_created', { item: req.body.item })
